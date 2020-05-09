a2c1affedc1f3aebc4bdb1b67e31a7ffaf7a26d9
Diego Ferreira <diego.lima3120@hotmail.com>
2020-05-09 15:51:37 -0300

    Atualizando os testes

--- src/test/java/br/com/tasks/funcional/TasksTest.java
+++ src/test/java/br/com/tasks/funcional/TasksTest.java
@@ -5,11 +5,9 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
