b67ea53c0f928dcf15479b7596f2f4808c12cb96
Diego Ferreira <diego.lima3120@hotmail.com>
2020-05-10 14:19:11 -0300

    pipe evolu

--- src/test/java/br/com/tasks/funcional/TasksTest.java
+++ src/test/java/br/com/tasks/funcional/TasksTest.java
@@ -37,7 +37,7 @@ public class TasksTest {
 		driver.findElement(By.id("saveButton")).click();
 		
 		String message = driver.findElement(By.id("message")).getText();
-		Assert.assertEquals("Sucess!", message);
+		Assert.assertEquals("Success!", message);
 		}finally {
 			driver.quit();
 		}
