bd8b1a30aa15db6f862809b0d56c56b07f7a7246
Diego Ferreira <diego.lima3120@hotmail.com>
2020-05-10 14:14:02 -0300

    corrigindo jenkinsfile

--- src/test/java/br/com/tasks/funcional/TasksTest.java
+++ src/test/java/br/com/tasks/funcional/TasksTest.java
@@ -8,6 +8,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -15,9 +16,9 @@ public class TasksTest {
 	
 	
 	public WebDriver acessarAplicacao() throws MalformedURLException {
-	//	WebDriver driver = new ChromeDriver();
-		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.15.10:4444/wd/hub"), cap);
+		WebDriver driver = new ChromeDriver();
+//		DesiredCapabilities cap = DesiredCapabilities.chrome();
+//		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.15.10:4444/wd/hub"), cap);
 		driver.navigate().to("http://192.168.15.10:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
